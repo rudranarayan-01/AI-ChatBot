1a0b76f333c6f3cae35e61f99c9187853025c08d
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-07-28 10:49:34 +0530

    agent is ready with changes in state

--- ai-agent.py
+++ ai-agent.py
@@ -1,39 +1,39 @@
-import dotenv
+from dotenv import load_dotenv
+import os
 
-# Step 1: Load environment variables from .env file
+# Load environment variables
+load_dotenv(".env")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-GROQ_API_KEY = dotenv.get_key(".env", "GROQ_API_KEY")
-TAVILY_API_KEY = dotenv.get_key(".env", "TAVILY_API_KEY")
-OPENAI_API_KEY = dotenv.get_key(".env", "OPENAI_API_KEY")
-
-# Check if the API keys are set
 if not GROQ_API_KEY or not TAVILY_API_KEY or not OPENAI_API_KEY:
-    raise ValueError("API keys are not set in the .env file. Please set GROQ_API_KEY, TAVILY_API_KEY, and OPENAI_API_KEY.")
+    raise ValueError("API keys are not set properly.")
 
-# Step 2: Setting up LLMS
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
-
-openai_llm = ChatOpenAI(model="gpt-4o-mini") 
-groq_llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=GROQ_API_KEY)
-search_tools = TavilySearchResults(max_results=2)
-
-# Step 3: Setting up AI Agnet with search tools functionality
 from langgraph.prebuilt import create_react_agent
 
-system_prompt = "Act as a helpful AI smart and friendly Chatbot that can search the web for information. You can use the Tavily search tool to find answers to questions that you don't know."
+openai_llm = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
+groq_llm = ChatGroq(model="llama3-70b-8192", api_key=GROQ_API_KEY)
+search_tools = TavilySearchResults(max_results=2)
+
+system_prompt = "Act as an AI chatbot who is smart and friendly"
 
 agent = create_react_agent(
     model=groq_llm,
     tools=[search_tools],
-    state_modifier = system_prompt,
 )
 
+
 query = "Tell me about the trends in crypto market"
-state = {"message": query}
-respose = agent.invoke(state)
-print(respose)
 
-# Step 4: Testing the agent with a query
-
+state = {
+    "messages": [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content":query}
+    ]
+}
+response = agent.invoke(state)
+print(response)
