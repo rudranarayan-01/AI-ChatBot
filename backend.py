52f2093223d29409ea47f6acd8a7f24896acf349
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-07-28 11:29:27 +0530

    adding alloed models

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@
 from pydantic import BaseModel
 from typing import List
 
-class RequestTest(BaseModel):
+class RequestState(BaseModel):
     model_name: str
     model_provider: str
     system_provider:str
@@ -12,5 +12,16 @@ class RequestTest(BaseModel):
     
 
 # Setp 2: Setup AI agent from frontend requests
+from fastapi import FastAPI
+
+ALLOWED_MODEL_NAMES = ["gpt-4o-mini", "llama3-70b-8192","mixtral-8x7b-32768", "lamma3.3-70b-versatile"]
+
+app = FastAPI(title="AI Agent API", description="API for AI Agent using LangChain and Groq", version="1.0.0")
+@app.post("/chat")
+
+def chat(request: RequestState):
+    """API endpoint to interact with chatbot using langgraph and search tools.
+    It dinamically selects the model based on the request.
+    """
 
 # Step 3: Run app and explore Swagger UI docs
