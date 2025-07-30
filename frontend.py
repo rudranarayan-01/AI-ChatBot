0f49ec776e9bbd9afa9d8e5f71238a8d550075fd
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-07-30 19:32:58 +0530

    streamlit page

--- frontend.py
+++ frontend.py
@@ -1,1 +1,6 @@
 # Step 1: Setup UI with streamlit(model provider, model, system prompt, query)
+import streamlit as st
+
+st.set_page_config(page_title="AI Agent", page_icon=":robot_face:", layout="wide")
+
+# Step 2: Connect backend with URL
