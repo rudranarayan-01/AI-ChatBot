79b0eac3cee1ad64b7f3de6deacbaef4c28e7974
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-07-30 19:39:36 +0530

    system prompt UI

--- frontend.py
+++ frontend.py
@@ -4,6 +4,8 @@ import streamlit as st
 st.set_page_config(page_title="AI Agent", page_icon=":robot_face:", layout="wide")
 st.title("AI Agent")
 st.write("Create and interact with AI agents.")
-st.sidebar.header("Settings")
+# st.sidebar.header("Settings")
+
+system_prompt = st.text_area("Define your AI agent", height=70,placeholder= "You are a helpful AI assistant.")
 
 # Step 2: Connect backend with URL
